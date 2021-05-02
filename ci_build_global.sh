1a2940a494471e551c1cb196170f214f7edd85ca
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-05-02 07:49:47 +0700

    Update ci_build_global.sh

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -8,7 +8,10 @@ if [ "$BUILD_IOS" != "1" ]; then
 fi
 . setdevkitpath.sh
 
-if [ "$BUILD_IOS" != "1" ]; then
+if [ "$BUILD_IOS" == "1" ]; then
+  chmod +x ios-arm64-clang
+  chmod +x ios-arm64-clang++
+else
   wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux-x86_64.zip"
   ./extractndk.sh
   ./maketoolchain.sh

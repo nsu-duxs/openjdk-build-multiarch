0e025f6733cf658834bc5e8d74ddbc2d4cadcdda
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-05-14 19:49:49 +0700

    [iOS build] set LDFLAGS

--- buildjdk.sh
+++ buildjdk.sh
@@ -47,6 +47,7 @@ else
   sameflags="-arch arm64 -isysroot $thesysroot -miphoneos-version-min=12.0 -DHEADLESS=1 -I$PWD/ios-missing-include -Wno-implicit-function-declaration"
   export CFLAGS+=" $sameflags"
   export CXXFLAGS="$sameflags"
+  export LDFLAGS="$sameflags"
   export CHOST="aarch64-apple-darwin"
 
   brew install xquartz

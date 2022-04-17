6d6c7fe799e4399cc44bbb35a87d746b86c2fd81
Boulay Mathias <mathiasboulay@free.fr>
2022-04-17 10:29:30 +0200

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -73,8 +73,8 @@ bash ./configure \
     --enable-option-checking=fatal \
     --with-jdk-variant=normal \
     --with-jvm-variants="${JVM_VARIANTS/AND/,}" \
-    --disable-aot \
-    --disable-fprof \
+    --disable-AOT \
+    --disable-FPROF \
     --with-cups-include=$CUPS_DIR \
     --with-devkit=$TOOLCHAIN \
     --with-debug-level=$JDK_DEBUG_LEVEL \

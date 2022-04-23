62069f9765aeb7b81972f676370390a0e3610a5a
Boulay Mathias <mathiasboulay@free.fr>
2022-04-23 21:52:52 +0200

    Update repackjre.sh

--- repackjre.sh
+++ repackjre.sh
@@ -56,7 +56,7 @@ makeuni () {
   rm lib/jexec;
   rm release;
   
-  rm -rf lib/ext 
+  find . ! -name 'zipfs.jar' -type f -exec rm -f {} +
   rm -rf lib/jfr
   rm lib/jfr.jar
   rm man/man1/servertool.1
